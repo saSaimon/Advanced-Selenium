5226e5b57511d776fe76e2d412f5daf30e97e2d9
saSaimon <asadiqulsaimon@gmail.com>
2024-09-17 22:40:39 +0600

    fresh start

--- App/Application.py
+++ App/Application.py
@@ -1,14 +1,5 @@
-from Pages.Login_Page import LoginPage
-from Pages.Home_Page import HomePage
-from Pages.Content_Page import ContentPage
-from Pages.Search_Page import SearchPage
-from Pages.Dashboard_Page import DashboardPage
+
 class Application:
 
     def __init__(self, driver):
         self.driver = driver
-        self.login_page = LoginPage(self.driver)
-        self.home_page = HomePage(self.driver)
-        self.content_page = ContentPage(self.driver)
-        self.search_page = SearchPage(self.driver)
-        self.dashboard_page = DashboardPage(self.driver)
--- Pages/Home_Page.py
+++
@@ -1,9 +0,0 @@
-from Pages.Base_Page import Page
-from selenium.webdriver.common.by import By
-
-
-class HomePage(Page):
-    HOME_LINK = (By.CSS_SELECTOR, '#__next > div > footer > div > ul > li:nth-child(1) > a')
-
-    def verify_home_text_present(self):
-        self.verify_text('Home', *self.HOME_LINK)
