d288017508084758893aa55ced803a58f3da75c1
sasaimon <asadiqulsaimon@gmail.com>
2024-10-27 14:58:39 +0600

    verification updated

--- Pages/Add_to_Cart_Page.py
+++ Pages/Add_to_Cart_Page.py
@@ -1,24 +1,45 @@
 from selenium.webdriver.common.by import By
 from Pages.Base_Page import Page
 
+
 class InventoryPage(Page):
     INVENTORY_ITEMS = (By.CLASS_NAME, "inventory_item")
     ADD_TO_CART_BUTTON = (By.XPATH, ".//button[normalize-space(text())='Add to cart']")
     REMOVE_BUTTON = (By.CSS_SELECTOR, '[class="btn btn_secondary btn_small btn_inventory "]')
     SHOPPING_CART = (By.CSS_SELECTOR, 'shopping_cart_container')
+    TITLES = (By.CLASS_NAME, 'inventory_item_name')
 
     def add_last_three_items_to_cart(self):
-
         items = self.find_elements(*self.INVENTORY_ITEMS)
         count = 0
+        last_titles = []
+
         for i in range(len(items) - 3, len(items)):
-            count = count+1
+            count += 1
             add_to_cart_button = items[i].find_element(*self.ADD_TO_CART_BUTTON)
             add_to_cart_button.click()
+            title = items[i].find_element(*self.TITLES).text
+            last_titles.append(title)
 
+        print(last_titles)
         remove_buttons = self.find_elements(*self.REMOVE_BUTTON)
         assert len(remove_buttons) == count
 
-    def click_shopping_cart(self):
+        return last_titles
 
+    def click_shopping_cart(self):
         self.open_url('https://www.saucedemo.com/cart.html')
+
+    def verify_last_three_items(self, last_titles):
+
+        product_titles_elements = self.find_elements(*self.TITLES)
+        titles = []
+        for title in product_titles_elements:
+            print(title.text)
+            titles.append(title.text)
+            assert title.text in last_titles
+
+
+
+
+
--- sauce_demo.py
+++ sauce_demo.py
@@ -1,6 +1,4 @@
 import unittest
-from time import sleep
-
 from selenium import webdriver
 from Pages.login_page import LoginPage
 from Pages.Add_to_Cart_Page import InventoryPage  # Import InventoryPage
@@ -17,19 +15,23 @@ class LoginTest(unittest.TestCase):
         self.checkout_page = CheckoutPage(self.driver)
 
     def test_login_and_add_items(self):
-
+        # Log in
         self.login_page.login_and_validate()
-        self.inventory_page.add_last_three_items_to_cart()
+
+        # Add the last three items to cart and get their titles
+        last_titles = self.inventory_page.add_last_three_items_to_cart()
+
+        # Go to cart and verify items
         self.inventory_page.click_shopping_cart()
+        self.inventory_page.verify_last_three_items(last_titles)
+
+        # Complete checkout process
         self.checkout_page.click_checkout_button()
         self.checkout_page.input_information()
         self.checkout_page.click_continue()
         self.checkout_page.click_finish()
 
-
-
     def tearDown(self):
-
         self.driver.quit()
 
 
